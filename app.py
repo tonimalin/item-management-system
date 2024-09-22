a70a5abc5377a93d401703a16192fdeb8ab67be4
Toni Malin <toni.malin@iki.fi>
2024-09-23 00:41:45 +0300

    Remove hard coded database address from app. Add database address to .env.

--- app.py
+++ app.py
@@ -2,10 +2,12 @@ from anytree import Node, RenderTree
 from flask import Flask
 from flask import redirect, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from os import getenv
 from sqlalchemy.sql import text
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///tmalin"
+#app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///tmalin"
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
 @app.route("/")
