da53fb8df9945a34876c77e981d2a2b1e173ec58
Toni Malin <toni.malin@iki.fi>
2024-10-19 03:44:33 +0300

    Add tables "item_locations" and "tasks" into schema.sql.

--- schema.sql
+++ schema.sql
@@ -1,17 +1,36 @@
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL,
+    password TEXT NOT NULL,
+    role TEXT NOT NULL
+);
+
 CREATE TABLE categories (
-id SERIAL PRIMARY KEY,
-category TEXT NOT NULL,
-parent INTEGER REFERENCES categories(id) ON DELETE SET NULL
+    id SERIAL PRIMARY KEY,
+    category TEXT NOT NULL,
+    parent INTEGER REFERENCES categories(id) ON DELETE SET NULL
 );
 
 CREATE TABLE locations (
-id SERIAL PRIMARY KEY,
-location TEXT NOT NULL,
-parent INTEGER REFERENCES locations(id) ON DELETE SET NULL
+    id SERIAL PRIMARY KEY,
+    location TEXT NOT NULL,
+    parent INTEGER REFERENCES locations(id) ON DELETE SET NULL
 );
 
-CREATE TABLE users (
-id SERIAL PRIMARY KEY,
-username TEXT NOT NULL,
-password TEXT NOT NULL,
-role TEXT NOT NULL);
+CREATE TABLE item_locations (
+    id SERIAL PRIMARY KEY,
+    category_id INTEGER REFERENCES categories(id) ON DELETE SET NULL,
+    location_id INTEGER REFERENCES locations(id) ON DELETE SET NULL,
+    notes TEXT
+);
+
+CREATE TABLE tasks (
+    id SERIAL PRIMARY KEY,
+    task TEXT,
+    owner_id INTEGER REFERENCES users(id) ON DELETE SET NULL,
+    status TEXT,
+    notes TEXT,
+    category_id INTEGER REFERENCES categories(id) ON DELETE SET NULL,
+    location_id INTEGER REFERENCES locations(id) ON DELETE SET NULL,
+    item_location_id INTEGER REFERENCES item_locations(id) ON DELETE SET NULL
+);
